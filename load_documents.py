c78ae349580a65b701339ba12752d99e048115a8
VishuddhaJatale <vibhu5016@gmail.com>
2026-01-22 15:22:12 +0530

    Fix PDF loader for Streamlit Cloud using PyMuPDF

--- load_documents.py
+++ load_documents.py
@@ -1,11 +1,12 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_community.document_loaders import DirectoryLoader, PyMuPDFLoader
 
 def load_documents(folder_path="data/uploaded_docs"):
 
     loader = DirectoryLoader(
-        folder_path,
-        glob="**/*.pdf",
-        loader_cls=PyPDFLoader
-    )
+    folder_path,
+    glob="**/*.pdf",
+    loader_cls=PyMuPDFLoader,
+    show_progress=True
+)
 
     return loader.load()
